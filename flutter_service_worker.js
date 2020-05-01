16f6efc18a2c9849e9a6ac1f630fbdcc583048b8
mohamedanwer <mohamedanwer006@gmail.com>
2020-05-01 16:29:54 +0200

    init

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,24 +1,25 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "fa59cb0ddff351c136afd518630f7bde",
+  "assets/AssetManifest.json": "73ce4179ef7d1951f11137b4620e4f83",
+"assets/assets/fonts/ConcertOne-Regular.ttf": "1b40f0186d1a8b1500b4676b2e9a7ba1",
+"assets/assets/fonts/Raleway-Regular.ttf": "9942588a6c84f959132556d99e83ded6",
 "assets/assets/images/ab-img.png": "8d7ae5ac6e934401e57e82f70b996bc1",
 "assets/assets/images/facebook.png": "ca84481b0e4b2de70aef09b26b6714b2",
 "assets/assets/images/github1.png": "bb15990c0ea25e2b725e7112baf218fd",
-"assets/assets/images/image.png": "f6cc6fc4a3c02b860265a804f845e925",
-"assets/assets/images/main-img.jpg": "93d3e31639a4d07613de9dccdc8bd5e8",
+"assets/assets/images/image.png": "dc61c02c18b94a762955aeab2d4ecc3c",
 "assets/assets/images/twitter.png": "400dc88500c0bc5e11f6c953dedcc7ab",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/FontManifest.json": "557d242da63aaa9d2f79e34c491e3373",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "05a4549c83654132e36fe65b3ce0d2c6",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/LICENSE": "ea584edd10f9b333f6121e61d34b1f09",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "9a62a954b81a1ad45a58b9bcea89b50b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "78103319d75cfc3b9f4b69433c71a616",
-"/": "78103319d75cfc3b9f4b69433c71a616",
-"main.dart.js": "a76b1edc1de1e6d67fc0c2a024dfc282",
-"manifest.json": "99a0e166c3c28b6d2d3b21266c9a58a8"
+"index.html": "c4bb6aa768c8cc1f3e9ab66623ea748d",
+"/": "c4bb6aa768c8cc1f3e9ab66623ea748d",
+"main.dart.js": "1ace69bff185f1370400844889fbfc23",
+"manifest.json": "a0c5a30bb262605e0de9d2da640d85ff"
 };
 
 self.addEventListener('activate', function (event) {
